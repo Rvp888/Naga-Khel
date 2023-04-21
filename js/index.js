4d649ed2badc212664fb38bec3c0b37b22cf39fc
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-21 21:50:35 +0530

    variables initiated in index.js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+let direction = {x: 0, y: 0};
+let foodSound = new Audio("../music/food.mp3");
+let gameOverSound = new Audio("../music/gameover.mp3");
+let moveSound = new Audio("../music/move.mp3");
+let musicSound = new Audio("../music/Bg-music.mp3");
+
