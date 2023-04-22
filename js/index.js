71c30ca64a635eea77c0bdec09f6fc2c7322223e
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-22 13:55:06 +0530

    functionalities and styles added

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-// Game constants & Variable
+// Game constants & Variables
 let inputDir = {x: 0, y: 0};
 const foodSound = new Audio("../music/food.mp3");
 const gameOverSound = new Audio("../music/gameover.mp3");
 const moveSound = new Audio("../music/move.mp3");
 const musicSound = new Audio("../music/Bg-music.mp3");
-let speed = 11;
+let speed = 5;
 let score = 0;
+let hiscoreval = 0;
 let lastPaintTime = 0;
 let snakeArr = [
     {x: 13, y: 15}
@@ -16,6 +17,7 @@ let food = {x: 6, y: 7}
 
 // Game Functions
 function main(ctime) {
+    // console.log("ctime", ctime);
     window.requestAnimationFrame(main);
     if((ctime - lastPaintTime)/1000 < 1/speed){
         return;
@@ -36,9 +38,11 @@ function isCollide(snake) {
         return true;
     }
     
+    return false;   
 }
 
 function gameEngine() {
+    // musicSound.play();
     // Part 1: Updating the snake array & Food
     if(isCollide(snakeArr)){
         gameOverSound.play();
@@ -48,12 +52,18 @@ function gameEngine() {
         snakeArr = [{x: 13, y: 15}];
         // musicSound.play();
         score = 0;
+        scoreBox.innerHTML = "Score: " + score;
     }
 
     // If you have eaten the food, increment the score and regenerate the food
     if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
         foodSound.play();
         score += 1;
+        if(score > hiscoreval){
+            hiscoreval = score;
+            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+            hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
+        }
         scoreBox.innerHTML = "Score: " + score;
         snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y})
         let a = 2;
@@ -93,6 +103,17 @@ function gameEngine() {
 
 
 // Main logic starts here
+// musicSound.play();
+let hiscore = localStorage.getItem("hiscore");
+if (hiscore === null){
+    hiscoreval = 0;
+    localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+}
+else {
+    hiscoreval = JSON.parse(hiscore);
+    hiscoreBox.innerHTML = "Hi-Score: " + hiscore;
+}
+
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e => {
     inputDir = {x: 0, y: 1} // Start the game
